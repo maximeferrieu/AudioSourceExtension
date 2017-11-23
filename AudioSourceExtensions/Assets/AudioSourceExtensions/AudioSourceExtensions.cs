6eb42338e102e4287bafcbc0c57615c73a65a77d
Maxime Ferrieu <maxime.ferrieu@gmail.com>
2017-11-23 12:31:16 +0100

    playOneShot() extensions

---
+++ AudioSourceExtensions/Assets/AudioSourceExtensions/AudioSourceExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AudioSourceExtensions
+{
+
+    public static void PlayOneShot(this AudioSource source, AudioClip clips, float volumeScale, float volumeRandomness)
+    {
+        int _indexToPlay = Random.Range(0, clips.Length);
+        AudioClip _clipToplay = clips[_indexToPlay];
+
+        float _volumeRandomValue = Random.Range(0f, volumeRandomness);
+
+        source.PlayOneShot(_clipToplay, volumeScale * _volumeRandomValue);
+    }
+
+    /// <summary>
+    /// Play a OneShot Random Sound from an array of AudioClips
+    /// </summary>
+    public static void PlayOneShot(this AudioSource source, AudioClip[] clips)
+    {
+        int _indexToPlay = Random.Range(0, clips.Length);
+        AudioClip _clipToplay = clips[_indexToPlay];
+        source.PlayOneShot(_clipToplay, 1f);
+    }
+
+    /// <summary>
+    /// Play a OneShot Random Sound from an array of AudioClips
+    /// </summary>
+    public static void PlayOneShot(this AudioSource source, AudioClip[] clips, float volumeScale)
+    {
+        int _indexToPlay = Random.Range(0, clips.Length);
+        AudioClip _clipToplay = clips[_indexToPlay];
+        source.PlayOneShot(_clipToplay, volumeScale);
+    }
+
+    /// <summary>
+    /// Play a OneShot Random Sound from an array of AudioClips
+    /// </summary>
+    public static void PlayOneShot(this AudioSource source, AudioClip[] clips, float volumeScale, float volumeRandomness)
+    {
+        int _indexToPlay = Random.Range(0, clips.Length);
+        AudioClip _clipToplay = clips[_indexToPlay];
+
+        float _volumeRandomValue = Random.Range(0f, volumeRandomness);
+
+        source.PlayOneShot(_clipToplay, volumeScale * _volumeRandomValue);
+    }
+}
